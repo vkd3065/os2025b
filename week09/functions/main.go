a3882964a2b417dec7245d5a75f7d0297f92e265
vkd3065 <vkd3065@gmail.com>
2025-10-27 15:33:06 +0900

    functions/main.go v0.3 package

--- week09/functions/main.go
+++ week09/functions/main.go
@@ -1,18 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func swap(first *int, second *int) {
-	temp := *first
-	*first = *second
-	*second = temp
-	fmt.Println(*first, *second)
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
 }
-
 func main() {
-	a, b := 10, 20
-	fmt.Println(a, b)
-	swap(&a, &b) // pass by pointer
-	fmt.Println(a, b)
-	// fmt.Println(math.Sqrt(-16.0))
+	fmt.Print("점수 입력 : ")
+	score, err := GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	status := ""
+	if score >= 90 {
+		status = "합격!"
+	} else {
+		status = "불합격"
+	}
+	fmt.Printf("%.2f점은 %v\n", score, status)
 }
