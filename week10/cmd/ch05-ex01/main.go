18d0b1df62b5c8afcdb877580a829e75b4bfe8b9
vkd3065 <vkd3065@gmail.com>
2025-11-03 15:47:36 +0900

    week10 array v0.6

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	arrayBool := [2]bool{true, false}
-	arrayInt := [3]int{-9, 11, 7}
-	for i := 0; i < len(arrayInt); i++ {
-		fmt.Println(i, arrayBool[i])
-		fmt.Println(i, arrayInt[i])
+	// numbers := [3]int{-9, 11, 7}
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(i, numbers[i])
+	// }
+	numbers := [3]int{-9, 11, 7}
+	for i, number := range numbers {
+		fmt.Println(i, number)
 	}
-	fmt.Printf("%#v\n", arrayBool)
-	fmt.Printf("%#v\n", arrayInt)
-	fmt.Println(reflect.TypeOf(arrayInt))
 }
