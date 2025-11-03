dcb1ac3b4a1ed998a57597ce309651a95b2c5968
vkd3065 <vkd3065@gmail.com>
2025-11-03 15:41:21 +0900

    week10 array v0.5

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	arrayBool := [3]bool{true, false, true}
-	var arrayInt [3]int
-	fmt.Println(arrayBool[1])
-	arrayInt[1] = 2
-	fmt.Println(arrayInt[1])
+	arrayBool := [2]bool{true, false}
+	arrayInt := [3]int{-9, 11, 7}
+	for i := 0; i < len(arrayInt); i++ {
+		fmt.Println(i, arrayBool[i])
+		fmt.Println(i, arrayInt[i])
+	}
+	fmt.Printf("%#v\n", arrayBool)
+	fmt.Printf("%#v\n", arrayInt)
+	fmt.Println(reflect.TypeOf(arrayInt))
 }
