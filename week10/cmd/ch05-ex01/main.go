ff5b6a9af26cdf436620d40d7a9dd3dacb544b71
vkd3065 <vkd3065@gmail.com>
2025-11-03 15:34:16 +0900

    week10 array v0.3

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var arrayBool [3]bool = [3]bool{true, false, true}
+	arrayBool := [3]bool{true, false, true}
 	var arrayInt [3]int
 	fmt.Println(arrayBool[1])
 	arrayInt[1] = 2
