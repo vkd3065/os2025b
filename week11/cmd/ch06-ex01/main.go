c11a8ebee2d6ab4184acc633dfa94df7fc34600d
vkd3065 <vkd3065@gmail.com>
2025-11-10 14:42:32 +0900

    week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -3,18 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// var subjects []string
-	// subjects = make([]string, 3)
-	// subjects[0] = "Go"
-	// subjects[2] = "Python"
-
-	// subjects := make([]string, 3)
-	// subjects[0] = "Go"
-	// subjects[2] = "Python"
-
-	subjects := []string{"Go", "", "Python"} // slice literal
-
+	subjects := [4]string{"Go", "Javascript", "Python", "Linux"}
+	subjectsSlice := subjects[1:3] // slicing
 	for _, subject := range subjects {
 		fmt.Println(subject)
+	}
+	fmt.Println("===============")
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
 	}
 }
