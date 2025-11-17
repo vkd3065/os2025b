6681c7029cd6b0b603facbc58380c9e98bdde57a
vkd3065 <vkd3065@gmail.com>
2025-11-17 15:07:11 +0900

    week12 struct v0.2

--- week12/ch08-ex01/main.go
+++ week12/ch08-ex01/main.go
@@ -14,7 +14,11 @@ func applyPrice(s *subcriber) {
 
 func main() {
 	var s1 subcriber
+	var p *subcriber = &s1
 	// s1.name = "Kim Inha"
 	applyPrice(&s1)
 	fmt.Println(s1.name, s1.price)
+	// fmt.Println(*p.price)
+	fmt.Println((*p).price)
+	fmt.Println(p.price)
 }
