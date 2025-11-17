5e060be87e9730abe773bbb899fc4bbbbbd550e2
vkd3065 <vkd3065@gmail.com>
2025-11-17 15:02:47 +0900

    week12 struct v0.1

--- week12/ch08-ex01/main.go
+++ week12/ch08-ex01/main.go
@@ -1,1 +1,20 @@
-d
+package main
+
+import "fmt"
+
+type subcriber struct {
+	name  string
+	price int
+}
+
+func applyPrice(s *subcriber) {
+	s.price = 10000
+	s.name = "Park Inha"
+}
+
+func main() {
+	var s1 subcriber
+	// s1.name = "Kim Inha"
+	applyPrice(&s1)
+	fmt.Println(s1.name, s1.price)
+}
