b1de7e543c58168cdf22ac2c63c51458b33a3791
vkd3065 <vkd3065@gmail.com>
2025-12-01 15:06:10 +0900

    week14 panic v0.3

--- week14/cmd/ch12-ex02/main.go
+++ week14/cmd/ch12-ex02/main.go
@@ -2,10 +2,27 @@ package main
 
 import "fmt"
 
+func safeDivide(a, b int) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("에러 발생:", err)
+		}
+	}()
+
+	if b == 0 {
+		panic("0으로 나눌 수 없습니다!")
+	}
+
+	result := a / b
+	fmt.Println("결과:", result)
+}
+
 func main() {
-	fmt.Println("1. 프로그램 시작")
+	fmt.Println("첫 번째 호출")
+	safeDivide(10, 2)
 
-	panic("심각한 에러 발생!")
+	fmt.Println("\n두 번째 호출")
+	safeDivide(10, 0)
 
-	fmt.Println("2. 이 줄은 실행되지 않음")
+	fmt.Println("\n프로그램 계속 실행됨")
 }
