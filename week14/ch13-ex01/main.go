b4107cb8400016e91a38737952ec99e36928a1e3
vkd3065 <vkd3065@gmail.com>
2025-12-01 15:16:59 +0900

    week14 goroutine v0.2

--- week14/ch13-ex01/main.go
+++ week14/ch13-ex01/main.go
@@ -8,11 +8,18 @@ import (
 func say(msg string) {
 	for i := 0; i < 3; i++ {
 		fmt.Println(msg, ":", i)
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(2000 * time.Millisecond)
 	}
 }
 
+func hi() {
+	fmt.Println("안녕")
+	time.Sleep(10 * time.Second)
+}
+
 func main() {
-	go say("고루틴") // 새 고루틴에서 실행
-	say("메인")     // 메인 고루틴에서 실행
+	start := time.Now()
+	hi()         // 새 고루틴에서 실행
+	go say("메인") // 메인 고루틴에서 실행
+	fmt.Println("전체 실행 시간 : ", time.Since(start))
 }
