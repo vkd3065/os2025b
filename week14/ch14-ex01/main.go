97b6480529306c2bc8ca9c621ea7ec990794a184
vkd3065 <vkd3065@gmail.com>
2025-12-01 15:47:57 +0900

    week14 goroutine v0.5

--- week14/ch14-ex01/main.go
+++ week14/ch14-ex01/main.go
@@ -2,13 +2,28 @@ package main
 
 import "fmt"
 
+func abc(channel chan string) {
+	channel <- "내고향 스페셜\n"
+	channel <- "KBS 뉴스광장\n"
+	channel <- "인간극장\n"
+}
+
+func def(channel chan string) {
+	channel <- "건강의 재구성 설록(재)\n"
+	channel <- "오늘N\n"
+	channel <- "찾아가는 꾸러기 교실\n"
+}
+
 func main() {
-	ch := make(chan int) // int 채널 생성
-
-	go func() {
-		ch <- 123 // 채널에 값 보내기
-	}()
-
-	x := <-ch // 채널에서 값 받기
-	fmt.Println(x)
+	kbs := make(chan string)
+	mbc := make(chan string)
+	go abc(kbs)
+	go def(mbc)
+	fmt.Print(<-kbs)
+	fmt.Print(<-mbc)
+	fmt.Print(<-kbs)
+	fmt.Print(<-mbc)
+	fmt.Print(<-kbs)
+	fmt.Print(<-mbc)
+	fmt.Println()
 }
